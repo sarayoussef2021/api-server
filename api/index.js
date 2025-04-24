f672aaa0d01d1a97707e7073e47b4c67009d635f
sarayoussef2021 <83817430+sarayoussef2021@users.noreply.github.com>
2025-04-24 23:35:41 +0100

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,17 +1,2 @@
-const express = require('express');
-const app = express();
-
-// Middleware pour gérer CORS et autres configurations
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://celadon-dusk-483980.netlify.app');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-  next();
-});
-
-// Exemple de route
-app.get('/', (req, res) => {
-  res.send('Hello, this is your API server!');
-});
-
+const app = require('../server');
 module.exports = app;
